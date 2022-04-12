e2c4b8ea75e2cd2762d25fb6d051b18905ed43a1
Elgars Upitis <elgars.upitis@gmail.com>
2022-04-12 13:41:10 +0300

    Complete exercise P8-08

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -2,6 +2,32 @@
 import java.util.HashMap;
 
 public class Program {
+    
+    public static void printKeys(HashMap<String, String> hashmap) {
+        for (String key : hashmap.keySet()) {
+            System.out.println(key);
+        }
+    }
+    
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(clean(text))) {
+                System.out.println(key);
+            }
+        }
+    }
+    
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(clean(text))) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+    }
+    
+    public static String clean(String input) {
+        return input.toLowerCase().trim();
+    }
 
     public static void main(String[] args) {
         // Test your program here!
