cdb7e3a6cb59ec2b48a7a7fdc79716b24969d9d7
Elgars Upitis <elgars.upitis@gmail.com>
2022-04-12 13:08:26 +0300

    Complete exercise P8-07

---
+++ part08-Part08_07.Abbreviations/src/main/java/Abbreviations.java
@@ -0,0 +1,28 @@
+
+import java.util.HashMap;
+
+public class Abbreviations {
+    
+    private HashMap<String, String> list;
+    
+    public Abbreviations() {
+        this.list = new HashMap<>();
+    }
+    
+    public static String clean(String input) {
+        String output = input.toLowerCase().strip();
+        return output;
+    }
+    
+    public void addAbbreviation(String abbreviation, String explanation) {
+        list.put(clean(abbreviation), explanation);
+    }
+    
+    public boolean hasAbbreviation(String abbreviation) {
+        return list.containsKey(clean(abbreviation));
+    }
+    
+    public String findExplanationFor(String abbreviation) {
+        return list.get(clean(abbreviation));
+    }
+}
