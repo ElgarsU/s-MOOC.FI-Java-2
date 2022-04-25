9edd4d6e92f86a89db0d69cc2fd643dda339d7c2
Elgars Upitis <elgars.upitis@gmail.com>
2022-04-25 12:42:42 +0300

    Complete exercise P10-06

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,18 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<String> list = new ArrayList<>();
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (Integer.valueOf(input) < 0) {
+                break;
+            }
+            list.add(input);
+        }
+        list.stream()
+                .mapToInt(number -> Integer.valueOf(number))
+                .filter(number -> ((number >= 1) && (number <= 5)))
+                .forEach(number -> System.out.println(number));
     }
 }
