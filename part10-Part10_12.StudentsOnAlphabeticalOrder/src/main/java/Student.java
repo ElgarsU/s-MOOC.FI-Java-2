dad8b24fc15dd145ccdc00d4a280b7251bfee567
Elgars Upitis <elgars.upitis@gmail.com>
2022-04-26 15:33:33 +0300

    Complete exercise P10-12

--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
@@ -1,6 +1,5 @@
 
-
-public class Student {
+public class Student implements Comparable<Student> {
 
     private String name;
 
@@ -15,6 +14,11 @@ public class Student {
     @Override
     public String toString() {
         return name;
+    }
+    
+    @Override
+    public int compareTo(Student other) {
+        return this.getName().compareToIgnoreCase(other.getName());
     }
 
 }
