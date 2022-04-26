91f8fc5a54bb099c791f8e433eabb5285fe8aaee
Elgars Upitis <elgars.upitis@gmail.com>
2022-04-26 17:18:22 +0300

    Complete exercise P10-13

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -3,10 +3,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class LiteracyComparison {
     
-    public static void main(String[] args) {
-
+    public static void main(String[] args)  {
+        
+        List<LiteracyData> data = new ArrayList<>();
+        
+        try {
+            Files.lines(Paths.get("literacy.csv"))
+                    .map(row -> row.split(","))
+                    .map(parts -> new LiteracyData(parts[3], Integer.valueOf(parts[4]), parts[2].replace(" (%)", "").trim(), Double.valueOf(parts[5])))
+                    .forEach(dataRow -> data.add(dataRow));
+           
+        } catch (Exception e) {
+            System.out.println("Error: " +e.getMessage());
+        }
+        
+        data.stream()
+                .sorted()
+                .forEach(entry -> System.out.println(entry));
     }
 }
