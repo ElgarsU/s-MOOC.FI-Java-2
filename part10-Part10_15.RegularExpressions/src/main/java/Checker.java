8a68d411840bfa49b64c9f56ddd412e65570bf1e
Elgars Upitis <elgars.upitis@gmail.com>
2022-04-27 10:31:31 +0300

    Complete exercise P10-15

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,28 @@
 
 
 public class Checker {
-
+    
+    public boolean isDayOfWeek(String string) {
+        return (string.matches("mon|tue|wed|thu|fri|sat|sun"));
+    }
+    
+    public boolean allVowels(String string) {
+        if (string.matches("[aeiou]*")) {
+            System.out.println("The form is correct.");
+            return true;
+        } else {
+            System.out.println("The form is incorrect.");
+            return false;
+        }
+    }
+    
+    public boolean timeOfDay(String string) {
+        if (string.matches("(0[1-9]|1[1-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]")) {
+            System.out.println("The form is correct.");
+            return true;
+        } else {
+            System.out.println("The form is incorrect.");
+            return false;
+        }
+    }
 }
