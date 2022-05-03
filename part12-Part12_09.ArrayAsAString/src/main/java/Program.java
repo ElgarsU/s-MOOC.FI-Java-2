ffbe9c8772a6285bcbc77ff860ac81774edf8e47
Elgars Upitis <elgars.upitis@gmail.com>
2022-05-03 12:08:47 +0300

    Complete exercise P12-09

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -4,5 +4,18 @@ public class Program {
     public static void main(String[] args) {
         // Test your method here
     }
+    
+    public static String arrayAsString(int[][] array) {
+        StringBuilder arrayString = new StringBuilder();
+        
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                arrayString.append(array[row][column]);
+            }
+            arrayString.append("\n");
+        }
+        
+        return arrayString.toString();
+    }
 
 }
